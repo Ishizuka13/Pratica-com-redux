19fbb8e83dec4d22d137ac1413d0d20f8f671eb4
Ishizuka13 <miguelishizuka4@gmail.com>
2023-04-22 13:37:47 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch } from "react-redux";
+import { setName, setAge } from "./redux/reducers/userReducer";
+
+import { useAppSelector } from "./redux/hooks/useAppSelector";
+import { setThemeStatus } from "./redux/reducers/themeReducer";
+
 
 function App() {
+  const dispatch = useDispatch();
+  const user = useAppSelector(state => state.user);
+  const theme = useAppSelector(state => state.theme);
+
+  const changeName = (newName: string) => dispatch(setName(newName));
+  const changeAge = (newAge: number) => dispatch(setName(newAge));
+  const switchTheme = (switchTheme: string) => dispatch(setThemeStatus(switchTheme));
+
+  const handleNameInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    changeName(e.target.value);
+  }
+  const handleAgeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    changeAge(parseInt(e.target.value));
+  }
+
+  const handleSwitchTheme = () => {
+    if(theme.status === 'light') {
+      switchTheme('dark');
+    } else {
+      switchTheme('light');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ 
+      backgroundColor: theme.status === 'light' ? '#FFF' : '#000',
+      color: theme.status === 'light' ? '#000' : '#FFF',
+      width: 1920,
+      height: 1080
+     }}>
+      Meu nome é: {user.name} e tenho {user.age} anos.<br/>
+      Tema: {theme.status}
+      <hr />
+      <input type='text' value={user.name} onChange={handleNameInput} />
+      <input type='text' value={user.age} onChange={handleAgeInput} />
+
+      <hr />
+      <button onClick={handleSwitchTheme}>Switch Theme</button>
     </div>
   );
 }
